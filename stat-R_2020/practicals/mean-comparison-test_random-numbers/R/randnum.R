26b896e8454704d1ea245a2e7a4b86915654fb5f
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2020-03-12 12:00:25 +0100

    added a time comparison bnetween for loop and replicate()

--- stat-R_2020/practicals/mean-comparison-test_random-numbers/R/randnum.R
+++ stat-R_2020/practicals/mean-comparison-test_random-numbers/R/randnum.R
@@ -53,8 +53,26 @@ dim(dataTable)
 # View(dataTable)
 
 
+#### Define metadata table ####
 
-boxplot(dataTable, horizontal = TRUE, las = 1)
+## Instantiate a data.frame with the group corresponding to each individual
+metaData <- data.frame(row.names = colnames(dataTable),
+                       group = c(rep(x = "g1", length.out = n1),
+                                 rep(x = "g2", length.out = n2)))
+## Add the replicate number
+metaData$replicate <- c(1:n1, 1:n2)
+
+## Associate a color to each group
+groupColors <- c("g1" = "#BBDDFF", "g2" = "#FFDDBB")                         
+metaData$color <- groupColors[as.vector(metaData$group)]
+
+## Generate a verbosy description
+metaData$description <- paste0("Group: ", metaData$group, 
+                               ", replicate: ", metaData$replicate)
+
+
+## Draw a beautiful boxplot
+boxplot(dataTable, horizontal = TRUE, las = 1, col = metaData$color)
 
 #### Craftwork approach of the hypothesis test #####
 
@@ -83,9 +101,12 @@ RandNumTest <- function(n1,
                         mu2 = 10,
                         sigma1 = 2,
                         sigma2 = 3,
+                        verbose = FALSE,
                         ...
                       ) {
-  message("Running random number test with n1 = ", n1, " and n2 = ", n2)
+  if (verbose) {
+    message("Running random number test with n1 = ", n1, " and n2 = ", n2)
+  }
   
   ## Print the message passed in the arguments
   x1 <- rnorm(n = n1, mean = mu1, sd = sigma1)
@@ -107,24 +128,68 @@ RandNumTest <- function(n1,
   return(resultVector)
 }
 
+## Store the column names in a vector, we will use it several times below
+randNumColNames <- c("m1", "m2", "d", "t", "p.value", "df")
 
 
-# RandNumTest() ## fails
+# RandNumTest() ## this fails
 RandNumTest(n1 = 4, n2 = 7, var.equal = FALSE, alternative = "two.sided")
 RandNumTest(n1 = 40, n2 = 70)
 
-print(testResult)
 
-print(n1)
+#### Run m tests (iterations) ####
 
-m <- 1000
-allResults <- data.frame(matrix(nrow=m, ncol = 6))
-colnames(allResults) <- c("m1", "m2", "d", "t", "p.value", "df")
-for (i in 1:m) {
-  allResults[i, ] <- RandNumTest(n1 = 16, n2 = 16, mu1 = 7, mu2 = 10,
-                                 var.equal = FALSE, alternative = "two.sided")
-}
+## Define the number of tests
+m <- 10000
+iterationResults <- data.frame(matrix(nrow = m, ncol = 6))
+colnames(iterationResults) <- randNumColNames
 
-hist(allResults$p.value)
+## Run m experiments and measure time with a loop (inefficient)
+message("iterating the test ", m, " times")
+iterationTime <- system.time(
+  for (i in 1:m) {
+    iterationResults[i, ] <- RandNumTest(n1 = 16, n2 = 16, mu1 = 7, mu2 = 10,
+                                         var.equal = FALSE, alternative = "two.sided")
+  }
+)
+print("Iteration time")
+print(iterationTime)
 
+## Slightly better: run the same test with replicate()
+message("iterating the test ", m, " times")
+replicateTime <- system.time(
+  repTests <- replicate(
+    n = m, 
+    RandNumTest(n1 = 16, 
+                n2 = 16, 
+                mu1 = 7, 
+                mu2 = 10, 
+                var.equal = FALSE, 
+                alternative = "two.sided")
+  )
+)
+class(repTests) ## this is a matrix
+dim(repTests)   ## m columns, one per test (feature)
 
+repTests <- t(repTests) ## Transpose the result because the vectors were aggregated as columns
+repTests <- data.frame(repTests) ## Convert the matrix into a data.frame
+class(repTests)  ## now a data.frame
+dim(repTests) ## we now have m rows, good !
+
+rownames(repTests) <- paste0("r", 1:m) ## name the rows
+colnames(repTests) <- randNumColNames  ## name the columns
+head(repTests)
+tail(repTests)
+print("Time with replicate()")
+print(replicateTime)
+
+colnames(repTests)
+
+## Draw the p-value histogram
+hist(repTests$p.value, breaks = 20)
+
+## Draw alpha
+alpha <- 0.05
+abline(v = alpha, col = "red")
+
+## 
