f28aa5a838a923fb8105f15cfcd3ae95b5d933e4
jvanheld <Jacques.van-Helden@univ-amu.fr>
2020-06-02 10:56:50 +0200

    comment

--- stat-R_2020/practicals/pavkovic_2019/R/01_reload_data.R
+++ stat-R_2020/practicals/pavkovic_2019/R/01_reload_data.R
@@ -1,19 +1,20 @@
 #### Reload the data from memory image ####
 
-datatype <- "transcriptome"
-dataset <- "fa"
+datatype <- "proteome"
+dataset <- "uuo"
 
 
 #### Define the path of the shared data dir ###
 
-## From inside the IFB cluster
+## Beware, the following row works only if you are logged in the IFB core cluster. 
+## If not see the next comment. 
 sharedDir <- "/shared/projects/dubii2020/data/module3/seance5"
 
 ## Note: if you are working on your own computer, you can download the memory image files from here 
 ## https://github.com/DU-Bii/module-3-Stat-R/tree/master/stat-R_2020/practicals/pavkovic_2019/memimages
 ## - click on the file of your choice
 ## - click Download
-## - adapt the sharedDir to specify the location of folder containing the memoryimage on your computed
+## - adapt the sharedDir to specify the location of folder containing the memory image on your computed
 
 ## List the files available in the shared data dir
 list.files(sharedDir)
